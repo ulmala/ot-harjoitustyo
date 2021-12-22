a251920b06411bfd0bfaacfb331688c3bdef5552
Olli Ulmala <ulmala.olli@hotmail.com>
2021-12-22 17:03:59 +0200

    fix window size

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ from ui.ui import UI
 def main():
     window = Tk()
     window.title('Yahtzee')
-    window.geometry('400x400')
+    window.geometry('450x450')
     window.resizable(0, 0)
     ui = UI(window)
     ui.start()
