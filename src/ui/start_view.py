123ca6e1722692ff16b68360c90d438a0b6c793f
Olli Ulmala <ulmala.olli@hotmail.com>
2021-12-26 12:02:10 +0200

    Add rules

--- src/ui/start_view.py
+++ src/ui/start_view.py
@@ -1,6 +1,6 @@
+import webbrowser
 from tkinter import ttk
 from services.game_service import game_service
-
 
 class StartView:
     def __init__(self, root, handle_game):
@@ -31,27 +31,38 @@ class StartView:
         header_label = ttk.Label(master=self._frame, text='Yahtzee', font=('TkDefaultFont', 30))
         header_label.grid(row=0, column=0, pady=(0,25))
 
+        self._initialize_link_to_rules()
+
         self._initialize_player_entries()
+
         start_button = ttk.Button(
             master=self._frame,text='Start',
             command=self._start_game_handler
         )
-
         start_button.grid(row=self._player_entries[-1].grid_info()['row'] + 1 , column=0)
-
-        label = ttk.Label(self._frame, text='All time top 5 scores')
-        label.grid(row=self._player_entries[-1].grid_info()['row'] + 2, column=0, pady=(25,0))
 
         self._initialize_top_scoreboard()
 
+    def _initialize_link_to_rules(self):
+        link_to_rules = ttk.Label(master=self._frame,
+                                 text='Click here to open game rules',
+                                 foreground='orange',
+                                 cursor='hand2', 
+                                 font=('TkDefaultFont 15 underline bold')
+        )
+        link_to_rules.grid(row=1, column=0, pady=(0,20))
+        link_to_rules.bind("<Button-1>", lambda x: webbrowser.open_new('https://en.wikipedia.org/wiki/Yahtzee#Rules'))
+
     def _initialize_player_entries(self):
-        for i in range(game_service.game.max_players):
-            player_label = ttk.Label(master=self._frame, text=f'Player {i+1}')
+        for i in range(1, game_service.game.max_players + 1):
+            player_label = ttk.Label(master=self._frame, text=f'Player {i}')
             self._player_entries.append(ttk.Entry(master=self._frame))
             player_label.grid(row=(i+1) * 2 - 1, column=0)
             self._player_entries[-1].grid(row=(i+1)*2, column=0)
 
     def _initialize_top_scoreboard(self):
+        label = ttk.Label(self._frame, text='All time top 5 scores')
+        label.grid(row=self._player_entries[-1].grid_info()['row'] + 2, column=0, pady=(25,0))
         self._top_scores = ttk.Treeview(master=self._frame,
                                         height=5,
                                         columns=['Player', 'Points'])
